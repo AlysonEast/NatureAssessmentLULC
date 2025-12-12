303f5c791186556990d8cbc33defcd3560f39af3
AlysonEast <a.east.ecology@gmail.com>
2025-12-12 11:12:17 -0500

    Updating to square miles instead of square KM

--- LCMAP.sh
+++ LCMAP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash                                                                     
 READDATA=0
-RECLASS=1
+RECLASS=0
 REPORTS=1
 
 export GISRC=/home/aly/.grassrc8.data
@@ -24,11 +24,11 @@ fi
 if [ $REPORTS -eq 1 ]
 then
 	#r.mask NLCD_Changed_2001_2021 --o
-	r.report map=LCMAP_CU_1985_V13_LCPRI,LCMAP_CU_2021_V13_LCPRI -h -n -a units=kilometers output=LCMAP_1985to2021_report
+	r.report map=LCMAP_CU_1985_V13_LCPRI,LCMAP_CU_2021_V13_LCPRI -h -n -a units=miles output=LCMAP_1985to2021_report --overwrite
 	awk 'BEGIN { FS="|"; } {print $2"	"$3"	"$5}' LCMAP_1985to2021_report >LCMAP_1985to2021_report.tsv
 
 	r.mask LCMAP_Changed_1985_2021 --o
-	r.report map=LCMAP_CU_1985_V13_LCPRI,LCMAP_CU_2021_V13_LCPRI -h -n -a units=kilometers output=LCMAPChanged_1985to2021_report
+	r.report map=LCMAP_CU_1985_V13_LCPRI,LCMAP_CU_2021_V13_LCPRI -h -n -a units=miles output=LCMAPChanged_1985to2021_report --overwrite
 	awk 'BEGIN { FS="|"; } {print $2"	"$3"	"$5}' LCMAPChanged_1985to2021_report >LCMAPChanged_1985to2021_report.tsv
 
 fi
